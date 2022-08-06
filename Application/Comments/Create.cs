1a81259d90209cf0609ebff0574cdd60b8d5d722
Erhan Basturk <56194980+basturkerhan@users.noreply.github.com>
2022-08-06 20:57:49 +0300

    Section 19 Completed

---
+++ Application/Comments/Create.cs
@@ -0,0 +1,76 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class Create
+    {
+        public class Command:IRequest<Result<CommentDto>>
+        {
+            public string Body { get; set; }
+            public Guid ActivityId { get; set; }
+        }
+
+        public class CommandValidator:AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(c => c.Body).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<CommentDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IUserAccessor userAccessor, IMapper mapper)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
+                if (activity == null) return null;
+
+                var user = await _context.Users
+                    .Include(p => p.Photos)
+                    .SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if(user == null) return null;
+
+                Comment comment = new Comment
+                {
+                    Body = request.Body,
+                    CreatedAt = DateTime.UtcNow,
+                    Activity = activity,
+                    Author = user
+                };
+
+                activity.Comments.Add(comment);
+                _context.Comments.Add(comment);
+                var success = await _context.SaveChangesAsync() > 0;
+
+                return success ?
+                    Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment)) :
+                    Result<CommentDto>.Failure("Failed to add comment");
+            }
+        }
+
+    }
+}
